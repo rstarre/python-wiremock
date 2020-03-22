b4eb4b81003596a1849ea884ec3bd1e0e99967f6
robert <r.vanderstarre@solcon.nl>
2020-03-22 11:03:27 +0100

    test env var

--- deploy_to_test_pypi.sh
+++ deploy_to_test_pypi.sh
@@ -2,7 +2,9 @@
 #
 # Deploy to PyPI for both source and wheel
 #
+
+echo $TWINE_USERNAME
 rm -Rf build/ dist/ wiremock.egg-info/ || true
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3 -m twine upload --non-interactive --repository-url https://test.pypi.org/legacy/ dist/*
 rm -Rf build/ dist/ wiremock.egg-info/ || true
